ae056b96fea3c7c368fbd68f87c7364c68741807
FaraiCH <faraichaka@gmail.com>
2024-04-14 17:07:42 +0200

    update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,9 +16,7 @@ class BookController extends Controller
     {
         $title = $request->input('title');
 
-        $books = Book::when($title, function ($query, $title){
-            return $query->title($title);
-        })->get();
+        $books = Book::when($title, fn($query, $title) => $query->title($title))->get();
 
         return view('books.index', ['books' => $books]);
     }
