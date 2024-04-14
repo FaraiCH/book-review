073abd71d5bf4f0d3bd3f94001e8b7df305f8e5d
FaraiCH <faraichaka@gmail.com>
2024-04-14 18:51:58 +0200

    update

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,6 +5,7 @@
 
     <form method="GET" action="{{route('books.index')}}" class="mb-4 flex items-center space-x-2">
         <input type="text" class="input" name="title" placeholder="Search by title" value="{{request('title')}}" />
+        <input type="hidden" name="filter" value="{{request('filter')}}">
         <button type="submit" class="btn">Search</button>
         <a href="{{route('books.index')}}" class="btn">Clear</a>
     </form>
@@ -21,7 +22,7 @@
     @endphp
 
     @foreach($filters as $key => $label)
-        <a href="#" class="{{request('filter')  === $key ? 'filter-item-active' : 'filter-item'}}">{{$label}}</a>
+        <a href="{{route('books.index', [...request()->query(), 'filter' => $key])}}" class="{{request('filter')  === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item'}}">{{$label}}</a>
     @endforeach
 </div>
 <ul>
